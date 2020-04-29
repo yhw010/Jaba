0f547b4253fb42cebd0254076aa095252bc84f06
bigdata <bigdata@DESKTOP-P2SSTCA>
2020-04-29 11:48:37 +0900

    2020/04/29 operatorTest.java수정

--- Ch03/src/sub1/OperatorTest.java
+++ Ch03/src/sub1/OperatorTest.java
@@ -9,8 +9,49 @@ public class OperatorTest {
 	
 	public static void main(String[] args) {
 		//산술연산자
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		int num4 = 4;
+		
+		int rs1 = num1 + num2;
+		int rs2 = num1 - num2;
+		int rs3 = num3 * num2;
+		int rs4 = num4 / num2;
+		int rs5 = 7% num2;//나눗셈 나머지를 구하는 연산자
+		
+		System.out.println("rs1 :"+rs1);
+		System.out.println("rs2 :"+rs2);
+		System.out.println("rs3 :"+rs3);
+		System.out.println("rs4 :"+rs4);
+		System.out.println("rs5 :"+rs5);
+		
 		//증강, 감소 연산자
+		int n1 = 1;
+		int n2 = 2;
+		
+		n1++;
+		n2--;
+
+		++n1;
+		--n2;
+		
+		System.out.println("n1의 값: "+n1);
+		System.out.println("n2의 값: "+n2);
+		
 		//복합대입연산자
+		int no1 = 1, no2 = 2, no3 = 3, no4 = 4;
+		
+		no1 += 1; //no1 = no1 +1;
+		no2 -= 2; //no2 = no2 -2;
+		no3 *= 3; //no3 = no3 *3;
+		no4 /= 4; //no4 = no4 /4;
+		
+		System.out.println("변수 no1의 값 :"+no1);
+		System.out.println("변수 no2의 값 :"+no2);
+		System.out.println("변수 no3의 값 :"+no3);
+		System.out.println("변수 no4의 값 :"+no4);
+		
 		//비교연산자
 		//논리연산자
 	}
