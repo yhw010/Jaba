e7b033a6f5d1c48cd3efab7273c7b64e6061a08c
bigdata <bigdata@DESKTOP-P2SSTCA>
2020-04-29 11:15:03 +0900

    2020/04/29 자바 Ch01 등록

---
+++ Ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,17 @@
+package sub1;
+
+/*
+ * 날짜 :2020/04/29
+ * 이름 : 윤재이
+ * 내용 : 자바 HelloWorld 출력하기
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("Hello Java!");
+	}
+	
+}
+
