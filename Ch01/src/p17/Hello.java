8bddabb885004bbf88557cfbbbc2d563b0d8cd77
yhw010 <yhw6054@naver.com>
2020-05-07 17:37:41 +0900

    2020/05/07  Ch01

---
+++ Ch01/src/p17/Hello.java
@@ -0,0 +1,14 @@
+package p17;
+
+/* 
+ 작성자: 윤재이
+ 작성일: 2020.05.07
+ 
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+	 //콘솔에 출력하는 실행문
+		System.out.println("Hello, welcom to the java world!");
+	} //end of main
+} //end of class
